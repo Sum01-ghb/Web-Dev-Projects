6dedc3e41e0843853b7fc7aac0cedce8689d338d
Sumit Patra <sumit500241@gmail.com>
2025-05-26 07:05:40 +0530

    Update App.jsx

--- E-Commerce App/Frontend/src/App.jsx
+++ E-Commerce App/Frontend/src/App.jsx
@@ -8,7 +8,7 @@ import Cart from "./pages/Cart.jsx";
 import Login from "./pages/Login.jsx";
 import PlaceOrder from "./pages/PlaceOrder.jsx";
 import Orders from "./pages/Orders.jsx";
-import Navbar from "./components/navbar.jsx";
+import Navbar from "./components/Navbar.jsx";
 import Footer from "./components/Footer.jsx";
 import SearchBar from "./components/SearchBar.jsx";
 import { ToastContainer, toast } from "react-toastify";
