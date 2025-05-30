442952fcd697a578919fb3cb9e537f6144320d6c
Sumit <sumit500241@gmail.com>
2025-05-30 22:13:01 +0530

    Completed admin dashboard and registration form

--- Hotel Booking/Client/src/App.jsx
+++ Hotel Booking/Client/src/App.jsx
@@ -3,6 +3,10 @@ import Navbar from "./components/Navbar.jsx";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Footer from "./components/Footer.jsx";
+import AllRooms from "./pages/AllRooms.jsx";
+import RoomDetails from "./pages/RoomDetails.jsx";
+import MyBookings from "./pages/MyBookings.jsx";
+import HotelReg from "./components/HotelReg.jsx";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -10,9 +14,13 @@ const App = () => {
   return (
     <div>
       {!isOwnerPath && <Navbar />}
+      {false && <HotelReg />}
       <div className="min-h-[70vh]">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/rooms" element={<AllRooms />} />
+          <Route path="/rooms/:id" element={<RoomDetails />} />
+          <Route path="/my-bookings" element={<MyBookings />} />
         </Routes>
       </div>
 
