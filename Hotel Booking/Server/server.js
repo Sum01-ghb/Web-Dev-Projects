e84b5fc69df48da95f7b331167b60b5548d49d49
Sumit <sumit500241@gmail.com>
2025-06-02 12:49:05 +0530

    created all api endpoints for backend

--- Hotel Booking/Server/server.js
+++ Hotel Booking/Server/server.js
@@ -5,8 +5,13 @@ import connectDB from "./configs/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import clerkWebhooks from "./controllers/clerkWebHooks.js";
 import userRouter from "./routes/userRoutes.js";
+import hotelRouter from "./routes/hotelRoutes.js";
+import connectCloudinary from "./configs/cloudinary.js";
+import roomRouter from "./routes/roomRoutes.js";
+import bookingRouter from "./routes/bookingRoutes.js";
 
 connectDB();
+connectCloudinary();
 
 const app = express();
 app.use(cors());
@@ -20,6 +25,9 @@ app.use("/api/clerk", clerkWebhooks);
 
 app.get("/", (req, res) => res.send("API is working"));
 app.use("/api/user", userRouter);
+app.use("/api/hotel", hotelRouter);
+app.use("/api/rooms", roomRouter);
+app.use("/api/bookings", bookingRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
