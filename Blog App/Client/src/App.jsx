4024c5c9b587ab61ab15076ad2f921c606b070eb
Sumit <sumit500241@gmail.com>
2025-06-10 15:23:08 +0530

    Admin panel completed

--- Blog App/Client/src/App.jsx
+++ Blog App/Client/src/App.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Blog from "./pages/Blog.jsx";
+import Layout from "./pages/Admin/Layout.jsx";
+import Dashboard from "./pages/Admin/Dashboard.jsx";
+import AddBlog from "./pages/Admin/AddBlog.jsx";
+import ListBlog from "./pages/Admin/ListBlog.jsx";
+import Comments from "./pages/Admin/Comments.jsx";
+import Login from "./components/Admin/Login.jsx";
+import "quill/dist/quill.snow.css";
 
 const App = () => {
   return (
@@ -9,6 +16,12 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />} />
+        <Route path="/admin" element={true ? <Layout /> : <Login />}>
+          <Route index element={<Dashboard />} />
+          <Route path="addBlog" element={<AddBlog />} />
+          <Route path="listBlog" element={<ListBlog />} />
+          <Route path="comments" element={<Comments />} />
+        </Route>
       </Routes>
     </div>
   );
