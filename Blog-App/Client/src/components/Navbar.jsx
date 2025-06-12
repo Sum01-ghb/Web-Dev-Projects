11be41bdc04e59fd9190d867feef3a4ab9649c9c
Sumit <sumit500241@gmail.com>
2025-06-12 14:05:07 +0530

    Update Navbar button text to specify 'Admin Login' for clarity

--- Blog-App/Client/src/components/Navbar.jsx
+++ Blog-App/Client/src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
         onClick={() => navigate("/admin")}
         className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5"
       >
-        {token ? "Dashboard" : "Login"}
+        {token ? "Dashboard" : "Admin Login"}
         <img src={assets.arrow} alt="arrow" className="w-3" />
       </button>
     </div>
