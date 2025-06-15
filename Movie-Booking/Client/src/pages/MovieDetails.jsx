430f57a30329ef7ed1217127d98754585e1ddb91
Sumit <sumit500241@gmail.com>
2025-06-15 13:57:17 +0530

    Completed home and movies page for movie booking website

---
+++ Movie-Booking/Client/src/pages/MovieDetails.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { dummyDateTimeData, dummyShowsData } from "../assets/assets.js";
+
+const MovieDetails = () => {
+  const { id } = useParams();
+
+  const [show, setShow] = useState(null);
+  const getShow = async () => {
+    const show = dummyShowsData.find((show) => show._id === id);
+    setShow({ movie: show, dateTime: dummyDateTimeData });
+  };
+
+  useEffect(() => {
+    getShow();
+  }, [id]);
+
+  return show ? <div></div> : <div>Loading</div>;
+};
+
+export default MovieDetails;
