7893bd7ad884b5e321d81d4bf2e026265ab448a4
Sumit <sumit500241@gmail.com>
2025-07-07 18:46:34 +0530

    Authentication endpoint added

--- Streamify/Backend/src/Routes/auth.route.js
+++ Streamify/Backend/src/Routes/auth.route.js
@@ -1,10 +1,21 @@
 import express from "express";
-import { login, logout, signup } from "../Controllers/auth.controller.js";
+import {
+  login,
+  logout,
+  onboard,
+  signup,
+} from "../Controllers/auth.controller.js";
+import { protectRoute } from "../Middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+
+router.post("/onboarding", protectRoute, onboard);
+router.get("/me", protectRoute, (req, res) => {
+  res.status(200).json({ success: true, user: req.user });
+});
 
 export default router;
