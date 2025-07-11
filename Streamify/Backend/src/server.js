70f6856abdfb9bfd8937832f7fe7676f022df92a
Sumit <sumit500241@gmail.com>
2025-07-11 14:29:43 +0530

    Deployment

--- Streamify/Backend/src/server.js
+++ Streamify/Backend/src/server.js
@@ -6,9 +6,12 @@ import chatRoutes from "./Routes/chat.route.js";
 import { connectDB } from "./Lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
@@ -17,6 +20,13 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
